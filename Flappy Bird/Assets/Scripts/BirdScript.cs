96af63ce05f24d2b1fee4912ad684e915c96f966
arghavan <arghavan.habibibibalani@gmail.com>
2023-03-18 16:26:49 +0330

    added the bird.

---
+++ Flappy Bird/Assets/Scripts/BirdScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    /// <summary>
+    /// This is the bird script.
+    /// </summary>
+
+    public Rigidbody2D myRigidBody;
+    public int jump ; // maybe i shoud change it to const value
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            myRigidBody.velocity = Vector2.up * jump;
+        }
+    }
+}
